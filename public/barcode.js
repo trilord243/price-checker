5dea1ca4c05646a246dc16705ecbcbb9ae724d7b
David E Lares S <david.e.lares@gmail.com>
2023-01-29 22:23:22 -0500

    Placeholder para integracion con barcode

--- public/barcode.js
+++ public/barcode.js
@@ -1,5 +1,41 @@
-console.log('barcode API implementation')
+var scannerArrayNew = [];
+var scannerArray
+// listing
+function listing(scannerArray) {
+  if(scannerArray.length > 0) {
+    for (i = 0; i < scannerArray.length; i++) {
+      if(scannerArray[i][2] == "SCANNER_CLASS_IMAGER" && scannerArray[i][1] == "SCANNER_INTERNAL") {
+        scannerInfo = scannerArray[i][0];
+        scannerArrayNew.push(scannerInfo);
+        break;
+      }
+    }
+  }
+}
 
+// retrieving
+function onDecodeEventJSON(jsonObject) {
+  console.log(jsonObject.data)
+  document.dispatchEvent(new CustomEvent('scanResult', {detail: jsonObject.data}))
+}
 
-// aqui va toda la logica que vas a revisar dentro del codigo fuente (HTML)
-// link: view-source:https://techdocs.zebra.com/enterprise-browser/samples/barcode/eb_barcode_api
+// enabling
+function disableCommonAPI() {
+  scanner.disable();
+}
+
+// enabling
+// enabling
+function enableCommonAPI() {
+  scanner.enable()
+}
+// start
+function startScanner() {
+  scanner.start();
+}
+// stop
+function stopScanner() {
+  scanner.stop();
+}
+// enumerating scanners
+enumeration()
