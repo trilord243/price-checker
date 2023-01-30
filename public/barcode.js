adc523d3ab33864730c6531b6b3752e352bd72a6
David E Lares S <david.e.lares@gmail.com>
2023-01-29 22:13:24 -0500

    Integrando Zebra API

---
+++ public/barcode.js
@@ -0,0 +1,5 @@
+console.log('barcode API implementation')
+
+
+// aqui va toda la logica que vas a revisar dentro del codigo fuente (HTML)
+// link: view-source:https://techdocs.zebra.com/enterprise-browser/samples/barcode/eb_barcode_api
